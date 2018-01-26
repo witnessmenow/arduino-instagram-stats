e9ae23627d31c7e6df45343fe01019374342e09c
witnessmenow <ecenuig@gmail.com>
2018-01-26 17:22:38 +0000

    Added example for ESP32

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # arduino-instagram-stats
-A library for fetching stats from Instagram on your arduino
+
+Get Instagram stats directly on your Arduino!
+
+![Instagram Library Example](https://i.imgur.com/wIsAlh5.png)
+
+Check out the examples for how to use it, no API key needed.
 
 This library is getting the data directly from Instagram, but it is not using their API.
-The API is pretty complicated to get access tokens for but found some information [here](https://gist.github.com/cosmocatalano/4544576) for a way to get JSON from public facing pages.
+The API is [pretty complicated to get access tokens for](https://medium.com/@bkwebster/how-to-get-instagram-api-access-token-and-fix-your-broken-feed-c8ad470e3f02) but I found some information [here](https://gist.github.com/cosmocatalano/4544576) for a way to get JSON from public facing pages.
 
 ## Currently Supports:
 
@@ -14,9 +19,9 @@ The API is pretty complicated to get access tokens for but found some informatio
 
 ### Tested Boards:
 - ESP8266
+- ESP32
 
 ### Should/Could Work:
-- ESP32
 - WiFi101 Boards (MKR1000 etc)
 
 ### Won't Work
---
+++ examples/esp32/UserData/UserData.ino
@@ -0,0 +1,72 @@
+/*******************************************************************
+ *  An example of usisng the InstagramStats library to get
+ *  info on a given user
+ *
+ *  Written by Brian Lough
+ *  https://www.youtube.com/channel/UCezJOfu7OtqGzd5xrP3q6WA
+ *******************************************************************/
+
+ // ----------------------------
+ // Standard Libraries - Already Installed if you have ESP32 set up
+ // ----------------------------
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+// ----------------------------
+// Additional Libraries - each one of these will need to be installed.
+// ----------------------------
+
+#include "InstagramStats.h"
+#include "JsonStreamingParser.h"
+
+//------- Replace the following! ------
+char ssid[] = "SSID";       // your network SSID (name)
+char password[] = "PASSWORD";  // your network key
+
+WiFiClientSecure client;
+InstagramStats instaStats(client);
+
+unsigned long delayBetweenChecks = 60000; //mean time between api requests
+unsigned long whenDueToCheck = 0;
+
+//Inputs
+String userName = "brian_lough"; // from their instagram url https://www.instagram.com/brian_lough/
+
+
+void setup() {
+
+  Serial.begin(115200);
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  IPAddress ip = WiFi.localIP();
+  Serial.println(ip);
+}
+
+void getInstagramStatsForUser() {
+  Serial.println("Getting instagram user stats for " + userName );
+  InstagramUserStats response = instaStats.getUserStats(userName);
+  Serial.println("Response:");
+  Serial.print("Number of followers: ");
+  Serial.println(response.followedByCount);
+}
+
+void loop() {
+  unsigned long timeNow = millis();
+  if ((timeNow > whenDueToCheck))  {
+    getInstagramStatsForUser();
+    whenDueToCheck = timeNow + delayBetweenChecks;
+  }
+}
