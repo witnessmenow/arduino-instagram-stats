21c9dd471c634afe6255c92116db9f84c508880b
apecker <adrian.pecker@preops.net>
2021-01-16 20:05:58 +0100

    New version of the Instagram Stats API using graph.facebook.com instead of doing a simple get to instagram user page which is now blocked by Instagram

--- src/InstagramStats.cpp
+++ src/InstagramStats.cpp
@@ -23,146 +23,101 @@ https://twitter.com/witnessmenow
 
 #include "InstagramStats.h"
 
-String currentKey = "";
-String currentParent = "";
-InstagramUserStats userStatsResponse;
-
-//**************************************************************************//
-// This code is the JSON Parser code written by squix78 as part of his example,
-// modified for this application //
-// https://github.com/squix78/json-streaming-parser //
-
-class InstagramUserListener : public JsonListener {
- public:
-  virtual void whitespace(char c);
-
-  virtual void startDocument();
-
-  virtual void key(String key);
-
-  virtual void value(String value);
-
-  virtual void endArray();
-
-  virtual void endObject();
-
-  virtual void endDocument();
-
-  virtual void startArray();
-
-  virtual void startObject();
-};
-
-void InstagramUserListener::whitespace(char c) {
-  // Serial.println("whitespace");
+InstagramStats::InstagramStats(Client &client, String accessToken, String igUserId = "") {
+	this->client = &client;
+  _accessToken = accessToken;
+	_igUserId = igUserId;
 }
 
-void InstagramUserListener::startDocument() {
-  // Serial.println("start document");
-}
+String InstagramStats::sendGetToInstagram(String command) {
+	String body;
+	String headers;
+	bool finishedHeaders = false;
+	bool currentLineIsBlank = true;
+	unsigned long now;
+	bool avail;
 
-void InstagramUserListener::key(String key) {
-  currentKey = key;
-}
+	// Connect with facebook api over ssl
+	if (client->connect(INSTA_HOST, INSTA_SSL_PORT)) {
+		if (_debug) Serial.println(".... connected to server");
+		String a="";
+		char c;
+		int ch_count=0;
+		client->println("GET " + command +  " HTTP/1.1");
+		client->println("Host: " INSTA_HOST);
+		client->println("User-Agent: arduino/1.0.0");
+		client->println("");
+		now=millis();
+		avail=false;
+		while (millis() - now < INSTA_TIMEOUT) {
+			while (client->available()) {
 
-void InstagramUserListener::value(String value) {
-  if (currentParent == "edge_followed_by") {
-    if (currentKey == "count") {
-      userStatsResponse.followedByCount = value.toInt();
-    }
-  }
-}
+				// Allow body to be parsed before finishing
+				avail = finishedHeaders;
+				char c = client->read();
+				// Serial.write(c);
 
-void InstagramUserListener::endArray() {
-  // Serial.println("end array. ");
-}
+				if(!finishedHeaders){
+					if (currentLineIsBlank && c == '\n') {
+						finishedHeaders = true;
+						//Serial.println(headers);
+					}
+					else{
+						headers = headers + c;
 
-void InstagramUserListener::endObject() {
-  currentParent = "";
-  // Serial.println("end object. ");
-}
+					}
+				} else {
+					if (ch_count < maxMessageLength)  {
+						body=body+c;
+						ch_count++;
+					}
+				}
 
-void InstagramUserListener::endDocument() {
-  // Serial.println("end document. ");
-}
-
-void InstagramUserListener::startArray() {
-  // Serial.println("start array. ");
-}
-
-void InstagramUserListener::startObject() {
-  currentParent = currentKey;
-  // Serial.println("start object. ");
-}
-//*********** END of Json Parser code **************//
-
-
-InstagramStats::InstagramStats(Client& client) {
-  this->client = &client;
-}
-
-InstagramUserStats InstagramStats::getUserStats(String user) {
-  if (_debug) Serial.println("starting getUserStats function");
-  JsonStreamingParser parser;
-  InstagramUserListener listener;
-  currentKey = "";
-  currentParent = "";
-  userStatsResponse = InstagramUserStats();
-  parser.setListener(&listener);
-
-  String sharedDataBuffer;
-  sharedDataBuffer.reserve(20);
-  String sharedData = "window._sharedData = ";
-  bool sharedDataFound = false;
-
-  long now;
-  // Connect to Instagram over ssl
-
-  // It's failing to connect ever second time on version 2.3 of ESP8266, so lets try this knonsense
-  if (client->connect(INSTA_HOST, INSTA_SSL_PORT) || client->connect(INSTA_HOST, INSTA_SSL_PORT)) {
-    if (_debug) Serial.println(".... connected to server");
-
-    client->println("GET /" + user + "/ HTTP/1.1");
-    client->print("Host:"); client->println(INSTA_HOST);
-    client->println();
-
-    now = millis();
-    while (millis() - now < 3000) {
-      while (client->available()) {
-        char c = client->read();
-
-
-        if (_debug) Serial.print(c);
-
-        if(!sharedDataFound) {
-          sharedDataBuffer = sharedDataBuffer + c;
-          if(sharedData.indexOf(sharedDataBuffer) == 0 ){
-            if (sharedData.length() == sharedDataBuffer.length()){
-              sharedDataFound = true;
-            }
-          } else {
-            sharedDataBuffer = "";
-          }
-        } else {
-          // parsing code:
-          // most of the work happens in the header code
-          // at the top of this file
-          parser.parse(c);
+				if (c == '\n') {
+					currentLineIsBlank = true;
+				}else if (c != '\r') {
+					currentLineIsBlank = false;
+				}
+			}
+			if (avail) {
+        // For some reason the body is coming back with a number on the outside of the start and end of it, we only want the JsonObject
+        int firstJsonCharacterIndex = body.indexOf("{");
+        int lastJsonCharacterIndex = body.lastIndexOf("}");
+        if(firstJsonCharacterIndex != -1 && lastJsonCharacterIndex != -1){
+          body = body.substring(firstJsonCharacterIndex, lastJsonCharacterIndex + 1);
         }
+				// Serial.println("Body:");
+				// Serial.println(body);
+				// Serial.println("END");
+				break;
+			}
+		}
+	}
+	else{
+		Serial.println(".... error connecting to server");
+	}
+	return body;
+}
 
-        // This is the check to see if you have everything you connected
-        // as the library is expanded to include more info this will connected
-        // to change.
-        if (userStatsResponse.followedByCount > 0) {
-          if (_debug) Serial.println("finished");
-          closeClient();
-          return userStatsResponse;
-        }
-      }
-    }
-  }
-  closeClient();
-  return userStatsResponse;
+int InstagramStats::getFollowersCount(String igUserId) {
+	String command = "/v9.0/" + igUserId + "?fields=followers_count&access_token=" + _accessToken;
+	String response = sendGetToInstagram(command);
+
+	if (_debug) Serial.println(response);
+
+  DynamicJsonBuffer jsonBuffer;
+	JsonObject& root = jsonBuffer.parseObject(response);
+
+	if(root.success()) {
+		if(root.containsKey("followers_count")) {
+			return root["followers_count"].as<int>();
+		} else {
+			Serial.println("JSON response was not as expected");
+		}
+  }	else {
+		Serial.println("Failed to parse JSON");
+	}
+  return -1;
 }
 
 void InstagramStats::closeClient() {
